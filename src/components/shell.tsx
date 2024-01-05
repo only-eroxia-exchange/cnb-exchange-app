57a296dc32a3ab269d509a80f73206f677647061
Lukas Hodac <zandom@seznam.cz>
2024-01-05 17:31:45 +0100

    error handle

--- src/components/shell.tsx
+++ src/components/shell.tsx
@@ -4,8 +4,9 @@ import Converter from './converter'
 import ListRates from './list-rates'
 
 export default function Shell() {
-  const { data } = useRateSourceInfoQuery()
+  const { data, isError } = useRateSourceInfoQuery()
 
+  if (isError) return <div>Error fetch. Try it again.</div>
   if (!data) return null
 
   return (
